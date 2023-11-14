0be8b19772dd587c5d94665a52156f15bcd931dc
sanlian114514 <147971457+sanlian114514@users.noreply.github.com>
2023-11-14 11:48:56 +0000

    Add files via upload

---
+++ bug.js
@@ -0,0 +1,9 @@
+function copy(){
+    const input = document.createElement('input');
+    document.body.appendChild(input);
+    input.setAttribute('value', "你干嘛~哎呦!");
+    input.select();
+    document.execCommand("copy");
+    document.body.removeChild(input);
+	alert("看看你的剪贴板!")
+}
