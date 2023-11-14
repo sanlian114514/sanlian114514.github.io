4db1bae7fa38a4f7a19c9c1a72f7b98b29094c5a
sanlian114514 <147971457+sanlian114514@users.noreply.github.com>
2023-11-14 19:49:26 +0800

    Delete copy.js

--- copy.js
+++
@@ -1,9 +0,0 @@
-function copy(){
-    const input = document.createElement('input');
-    document.body.appendChild(input);
-    input.setAttribute('value', "sanlian114514{$rthSuffix}的主人泰裤辣!");
-    input.select();
-    document.execCommand("copy");
-    document.body.removeChild(input);
-	alert("看看你的剪贴板!")
-}
